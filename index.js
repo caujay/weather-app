83ca8c2b71f960e30fbcffbe35184f2cad29690b
Karol Antoszewski <karolantoszewski@Karols-MacBook-Pro.local>
2021-06-22 17:00:59 +0200

    Added dynamic city cards fetching data from API

--- index.js
+++ index.js
@@ -1,22 +1,34 @@
 const wrapContainer = document.querySelector(".wrap-container");
-console.log(wrapContainer);
+const cityInput = document.querySelector(".city-input");
+const citiesWrapper = document.querySelector(".cities-wrapper");
+const submitBtn = document.querySelector("#submit-btn");
+const cityName = document.querySelector(".city-name");
+const checkWeatherBtn = document.querySelector(".check-weather-btn");
 
-const delBtn = document.querySelector(".del-btn");
+checkWeatherBtn.addEventListener("click", () => {
+  fetch(
+    `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=7G7aXU4h0HhOxh3j6pXbef6cmg4NGt6a&q=${cityName.value}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      if (data.length > 1) {
+        console.log("jest wiecej miast niz 1");
+      }
+      console.log(data);
+      const cityInformations = `<span class="city-name">${data[0].EnglishName}</span>
+        <img class="weather" alt="" src="" />
+        <span class="temperature"></span>
+        <button class="del-btn">-</button>`;
+      const newCityTab = document.createElement("div");
+      newCityTab.className = "city-container";
+      newCityTab.innerHTML = cityInformations;
+      citiesWrapper.appendChild(newCityTab);
+    });
+});
 
-delBtn.addEventListener( 'click', function() {
-    
-}
-
-fetch("https://api.oceandrivers.com:443/v1.0/getAemetStation/lodz/lastdata/")
-  .then((res) => res.json())
-  .then((data) => {
-    console.log(data);
-    const cityTab = `<div class="city-container">
-    <span class="city-name">Lodz</span>
-    <img class="weather" alt="" src="" />
-    <span class="temperature">${data.TEMPERATURE}</span>
-    <button class="del-btn">-</button>
-    </div>`;
-    wrapContainer.innerHTML = cityTab;
-  });
-  
+wrapContainer.onclick = (e) => {
+  let target = e.target;
+  if (target === document.querySelector(".del-btn")) {
+    target.parentNode.remove();
+  }
+};
